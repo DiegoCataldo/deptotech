0ded8f777ee60d013b25008e5d486f57f7ae2084
priceanswers <priceanswers@gmail.com>
2022-01-11 21:23:24 -0300

    port updated

--- src/public/js/main2.js
+++ src/public/js/main2.js
@@ -179,7 +179,7 @@ $(function () {
   // jQuery counterUp (used in Facts section)
   $('[data-toggle="counter-up"]').counterUp({
     delay: 10,
-    time: 1000
+    time: 200
   });
 
   // Porfolio isotope and filter
